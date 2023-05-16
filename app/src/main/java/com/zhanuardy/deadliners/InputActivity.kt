dfb29540976f67d3089c595ad9ebe27dbb7d09cc
touille3222 <wush3222@gmail.com>
2023-05-17 02:34:51 +0700

    add date selector for deadline and submit button

--- app/src/main/java/com/zhanuardy/deadliners/InputActivity.kt
+++ app/src/main/java/com/zhanuardy/deadliners/InputActivity.kt
@@ -1,14 +1,49 @@
 package com.zhanuardy.deadliners
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.widget.EditText
+import com.zhanuardy.deadliners.databinding.ActivityInputBinding
+import com.zhanuardy.deadliners.databinding.ActivityMainBinding
+import java.util.*
 
 class InputActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.dateAktivitasEditText.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                binding.dateAktivitasEditText.setText(selectedDate)
+            }, year, month, day)
+
+            datePickerDialog.show()
+        }
+
+        binding.jameditTextTime.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+
+            val timePickerDialog = TimePickerDialog(this, { _, selectedHour, selectedMinute ->
+                val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute)
+                binding.jameditTextTime.setText(selectedTime)
+            }, hour, minute, true)
+
+            timePickerDialog.show()
+        }
     }
 }
